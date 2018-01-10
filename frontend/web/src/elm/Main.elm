74443e108d72429f36cc27e1ebf2a860d27cdd6a
Olivier Melcher <olivier.melcher@gmail.com>
2018-01-10 00:57:07 -0600

    add model state

--- frontend/web/src/elm/Main.elm
+++ frontend/web/src/elm/Main.elm
@@ -3,7 +3,7 @@ port module Main exposing (..)
 import Cognito
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onSubmit)
+import Html.Events exposing (onSubmit, onInput)
 
 
 main : Program Never Model Msg
@@ -22,18 +22,31 @@ main =
 
 
 type alias Model =
-    { signupForm :
-        { email : String
-        , password : String
-        }
+    { signupFlow : FormState
     }
+
+
+type FormState
+    = SignupForm
+            { email : String
+            , password : String
+            , isSubmitting : Bool
+            , error : Maybe String
+            }
+    | SignupConfirmationForm
+            { username : String
+            , code : String
+            , isSubmitting : Bool
+            , error : Maybe String
+            }
+    | SignedUp
 
 
 initialModel : Model
 initialModel =
     { signupForm =
-        { email = "elm-cognito@mailinator.com"
-        , password = "password"
+        { email = "elmcognito-authn-dev-01@mailinator.com"
+        , password = "AllThe$3kr37s"
         }
     }
 
@@ -46,6 +59,8 @@ type Msg
     = DoSignup
     | CognitoError String
     | CognitoSignupSuccess { username : String }
+    | SignupEmailChanged String
+    | SignupPasswordChanged String
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
@@ -62,6 +77,11 @@ update msg model =
         CognitoSignupSuccess _ ->
             ( model, Cmd.none )
 
+        SignupEmailChanged _ ->
+           ( model, Cmd.none )
+
+        SignupPasswordChanged _ ->
+           ( model, Cmd.none )
 
 
 -- view
@@ -74,9 +94,19 @@ view model =
             [ h1 []
                 [ text "Welcome" ]
             , Html.form [ class "form", onSubmit DoSignup ]
-                [ input [ placeholder "Email", type_ "text", defaultValue model.signupForm.email ]
+                [ input
+                    [ placeholder "Email"
+                    , type_ "text"
+                    , defaultValue model.signupForm.email
+                    , onInput SignupEmailChanged
+                    ]
                     []
-                , input [ placeholder "Password", type_ "password", defaultValue model.signupForm.email ]
+                , input
+                    [ placeholder "Password"
+                    , type_ "password"
+                    , defaultValue model.signupForm.email
+                    , onInput SignupPasswordChanged
+                    ]
                     []
                 , button [ id "signup-button", type_ "submit" ]
                     [ text "Signup" ]
